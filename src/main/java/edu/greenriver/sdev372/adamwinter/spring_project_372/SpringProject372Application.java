fc2cd7d231487a5f3714ec0f6cee2dd5f0c37331
AdamZWinter <adam@bootstrapmusic.com>
2023-10-03 13:19:38 -0700

    First Commit

---
+++ src/main/java/edu/greenriver/sdev372/adamwinter/spring_project_372/SpringProject372Application.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev372.adamwinter.spring_project_372;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProject372Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProject372Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/greenriver/sdev372/adamwinter/spring_project_372/SpringProject372ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev372.adamwinter.spring_project_372;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject372ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
