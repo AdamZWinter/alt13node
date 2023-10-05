1cca48822e6859ca4aa6b65e37fe51632be58e79
AdamZWinter <adam@bootstrapmusic.com>
2023-10-05 10:14:19 -0700

    saving work

--- src/test/java/edu/greenriver/sdev372/adamwinter/spring_project_372/SpringProject372ApplicationTests.java
+++ src/test/java/edu/greenriver/sdev372/adamwinter/spring_project_372/SpringProject372ApplicationTests.java
@@ -1,13 +1,21 @@
 package edu.greenriver.sdev372.adamwinter.spring_project_372;
 
+import edu.greenriver.sdev372.adamwinter.spring_project_372.models.Account;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class SpringProject372ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void accountTest(){
+		Account account = new Account("emailAddress");
 	}
 
 }
